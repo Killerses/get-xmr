c71759b8f1e92b787e9cd25a9c991bbb9a9ee7e5
Killerses <107223246+Killerses@users.noreply.github.com>
2022-06-10 12:50:40 +0200

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 45yndLvbPcpKuFDZdE6iVh7LokjYmsoy6QBNyBZnFbcobnnXt1d8ktNHxyggiJV3d21wBtpQGuukvBzz3mVvKV78AJ8XVT7 -k --tls --rig-id GC-US
